a6b30d5e971557866b8ed9d44d1211609611cf1f
maquintanarp-tech <maquintanarp@gmail.com>
2025-11-26 13:29:53 +0100

    Add Fahrenheit to Celsius conversion in hello-world.java

--- hello-world.java
+++ hello-world.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
 
-// Projecto de prueba en Entornos
+public class actividad7 {
+     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int fr;
+    System.out.println(" Cuantos grados farenhait ? ");
+    fr = sc.nextInt();
+    int grados = fr -32;
+    int fr1 = fr * 5;
+    double gradoscelcius =(double) fr1 /9;
+    System.out.println(  " Tus grados celcius son " + gradoscelcius );
+
+
+}
+   }
